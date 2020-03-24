90e17052fa94222663e0004193d17979c1fbe86e
Tatsunomiya <black.r.d@icloud.com>
2020-03-24 21:11:56 +0900

    test2

---
+++ pleiades-4.6.3-java-win-64bit-jre_20170422/pleiades/workspace/tasklist/WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+
+
+
+
+<label for="task">タスク</label><br />
+<input type="text" name="tasks" value="${tasks.task}" />
+
+<br /><br />
+
+
+<input type="hidden" name="_token" value="${_token}" />
+
+<button type="submit"> 投稿</button>
--- pleiades-4.6.3-java-win-64bit-jre_20170422/pleiades/workspace/tasklist/src/controllers/ShowServlet.java
+++ pleiades-4.6.3-java-win-64bit-jre_20170422/pleiades/workspace/tasklist/src/controllers/ShowServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,16 +14,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public ShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,18 +31,29 @@ public class IndexServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> task = em.createNamedQuery("getAllTasks", Task.class)
-                                   .getResultList();
 
-        em.close();
+	    Task m = em.find(Task.class,Integer.parseInt(request.getParameter("id")));
 
-        request.setAttribute("tasks", task);
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-        rd.forward(request, response);
-    }
+	    em.close();
+
+
+	    request.setAttribute("task", m);
+
+
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
+
+	    rd.forward(request, response);
+
+
+
+
+
+
+	}
 
 }
